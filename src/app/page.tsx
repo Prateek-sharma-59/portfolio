096113725a95aafa18c2286696f0f1cc257c6ba4
Prateek Sharma <prateek591997@gmail.com>
2025-01-31 14:27:23 +0530

    prateek

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import About from "@/components/about";
 import Experience from "@/components/experience";
 import LandingComponent from "@/components/landingComponent";
-import Image from "next/image";
 
 export default function Home() {
   return (
